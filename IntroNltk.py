22eaff108d2c9a8decc748777e479f55706306b2
amoghli13 <amoghli13@gmail.com>
2015-12-22 23:31:35 -0800

    Adding intro nltk script

---
+++ IntroNltk.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+#Tutorial found at  http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
+
+from nltk.probability import ELEProbDist, FreqDist
+from nltk import NaiveBayesClassifier
+import nltk
+
+pos_tweets = [('I love this car', 'positive'),
+              ('This view is amazing', 'positive'),
+              ('I feel great this morning', 'positive'),
+              ('I am so excited about the concert', 'positive'),
+              ('He is my best friend', 'positive')]
+
+neg_tweets = [('I do not like this car', 'negative'),
+              ('This view is horrible', 'negative'),
+              ('I feel tired this morning', 'negative'),
+              ('I am not looking forward to the concert', 'negative'),
+              ('He is my enemy', 'negative')]
+
+tweets = []
+for (words, sentiment) in pos_tweets + neg_tweets:
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+    tweets.append((words_filtered, sentiment))
+
+test_tweets = [
+    (['feel', 'happy', 'this', 'morning'], 'positive'),
+    (['larry', 'friend'], 'positive'),
+    (['not', 'like', 'that', 'man'], 'negative'),
+    (['house', 'not', 'great'], 'negative'),
+    (['your', 'song', 'annoying'], 'negative')]
+
+
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+        all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+word_features = get_word_features(get_words_in_tweets(tweets))    
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+def train(labeled_featuresets, estimator=ELEProbDist):
+    # Create the P(label) distribution
+    label_probdist = estimator(label_freqdist)
+    # Create the P(fval|label, fname) distribution
+    feature_probdist = {}
+    return NaiveBayesClassifier(label_probdist, feature_probdist)
+
+for (currTestTweet,realSenti) in test_tweets:
+    print "tweet: %s sentiment-- pred: %r actual: %r "%(currTestTweet,classifier.classify(extract_features(currTestTweet)),realSenti)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SentimentAnalysis
 Survey of projects on twitter sentiment analysis
+
+1. Simple example of sentiment analysis using NLTK,NaiveBayesClassifier as shown in
+http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
+
+
